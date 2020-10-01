1ce1ca0f1f21ab678080da9973710a39b50fd31b
Guilherme <guilhermevictor20@gmail.com>
2020-10-01 11:52:05 -0300

    curso-typescript: object-type

---
+++ curso-typescript/src/object-type/index.ts
@@ -0,0 +1,14 @@
+const objectA: {
+  keyA: string;
+  keyB: string;
+  [key: string]: unknown;
+} = {
+  keyA: 'Guilherme',
+  keyB: 'Victor',
+};
+
+objectA.keyA = 'Value';
+objectA.keyB = 'Other Value';
+objectA.keyC = 'Test';
+
+console.log(objectA.keyC);
