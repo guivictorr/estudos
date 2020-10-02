78ccf2708799654c0efb36f560a3c1e6609fbc6c
Guilherme <guilhermevictor20@gmail.com>
2020-10-02 11:29:02 -0300

    curso-typescript: tuple-type

---
+++ curso-typescript/src/tuple-type/index.ts
@@ -0,0 +1,7 @@
+const clientData: [number, ...string[]] = [1, 'Luiz', 'Carlos'];
+const clientData1: [number, string?] = [1];
+const clientData3: readonly [number, string] = [1, 'Guilherme'];
+
+console.log(clientData);
+console.log(clientData1);
+console.log(clientData3);
