df2b4264b32264cd7f9a0ed5e0139c573515f4fa
Guilherme <guilhermevictor20@gmail.com>
2020-10-02 11:47:30 -0300

    curso-typescript: enum-type

---
+++ curso-typescript/src/enum-type/index.ts
@@ -0,0 +1,18 @@
+enum Colors {
+  RED, // 0
+  BLUE, // 1
+  YELLOW, // 2
+}
+
+console.log(Colors);
+console.log(Colors.BLUE);
+
+console.log('=============================================');
+
+enum Colors {
+  GREEN = 'GREEN',
+  PINK = 'PINK',
+  BLACK = 'BLACK',
+}
+
+console.log(Colors);
